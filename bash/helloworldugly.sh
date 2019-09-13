17b92282ed00e7990dccb8ea525cfc5c4818b8cd
COMP2101ht <ngochieu471998@gmail>
2019-09-13 10:32:45 -0400

    edit

--- bash/helloworldugly.sh
+++ bash/helloworldugly.sh
@@ -2,7 +2,7 @@
 # My third script - helloworldugly.#!/bin/sh
 # This is a silly way of creating the output text by starting with something else and stream editing it in a pineline
 echo -n "helb wold" |
-  sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl" |
+  sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl/" |
   tr "h" "H"|
   tr "w" "W"|
   awk '{print $1 "\x20" $2 "\41"}'
