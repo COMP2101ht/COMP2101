b29439b6216a3c26e69980f15d941e28c74b56f5
COMP2101ht <ngochieu471998@gmail>
2019-09-20 08:54:36 -0400

    Labs

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -1,2 +1,15 @@
-wget -0 ~/COMP2101/bash/findsetidfiles.sh https://zonzorp.github.io/COMP2101/Labs/bash/scripts-lab1/findsetdifiles.sh
-chmod +x ~/COMP2101/bash/findsetdifiles.sh
+#!bin/bash
+#
+# this script generates a report of the files on the system that have the setuid permission bit turn on
+#it is for the lab1 exercise
+
+#Task 1 for the exercise is to modify it to also find and display the setgit files in a second listing
+#The second listing should display after the setuid fils list, and be formatted similar to the setuid file list
+
+#Task 2 for the exercise is to modify it to also display the 10 largest files in the system, sorted by their sizes
+# THe listing should include the file name, owner, and size in MBytes and be displayed after the listings of setuid and setgid files
+
+echo "Setuid files:"
+echo "============="
+find / -type -executable -perm -4000 -ls 2>/dev/null | sort -k 3
+echo ""
