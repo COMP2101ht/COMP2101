63f18bb3ad9004adde69de97d98a8176b96134ad
COMP2101ht <ngochieu471998@gmail>
2019-09-24 14:10:45 -0400

    edit

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 #
 # this script generates a report of the files on the system that have the setuid permission bit turn on
 #it is for the lab1 exercise
@@ -10,6 +10,14 @@
 # THe listing should include the file name, owner, and size in MBytes and be displayed after the listings of setuid and setgid files
 
 echo "Setuid files:"
+
 echo "============="
-find / -type -executable -perm -4000 -ls 2>/dev/null | sort -k 3
-echo ""
+find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
+
+echo "Setgit files:"
+
+echo "============="
+find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 3
+
+echo "10 largest files"
+find / 2>/dev/null -type f -exec ls -l --block-size=M {} + | sort -rh -k 5 | head -n 10 
