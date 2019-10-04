b78b921dc3158008aab63a0b50c5a3b403c07011
COMP2101ht <ngochieu471998@gmail>
2019-10-04 09:05:53 -0400

    done

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -9,17 +9,6 @@
 #   - first divided by second gives X with a remainder of Y
 # Improve this script by calculating and displaying the first number raised to the power of the second number
 
-read -p "Enter a number: " n1
-read -p "Enter a second number: " n2
-
-echo "Subtraction of $n1 - $n2 is      = " $((n1-n2))
-echo "Multiplication of $n1 * $n2 is   = " $((n1*n2))
-echo "Modulus of $n1 % $n2 is          = " $((n1%n2))
-
-n3=$((n1/n2))
-remainder=$((n1%n2))
-echo "n1 divided n2 gives $n3 with a remainder of $remainder"
-echo "The first number $n1 raised to the power of the second number $n2 is = " $((n1**n2))
 
 firstnum=5
 secondnum=2
@@ -32,3 +21,21 @@ $firstnum plus $secondnum is $sum
 $firstnum divided by $secondnum is $dividend
   - More precisely, it is $fpdividend
 EOF
+
+echo "-------IMPROVE---------"
+# Improve this script by asking the user to supply the two numbers
+read -p "Enter a number: " n1
+read -p "Enter a second number: " n2
+
+# Improve this script by demonstrating using subtraction and multiplication
+echo "Subtraction of $n1 - $n2 is      = " $((n1-n2))
+echo "Multiplication of $n1 * $n2 is   = " $((n1*n2))
+
+# Improve this script by demonstrating the modulus operation
+echo "Modulus of $n1 % $n2 is          = " $((n1%n2))
+n3=$((n1/n2))
+remainder=$((n1%n2))
+echo "The first number divided the second gives $n3 with a remainder of $remainder"
+
+# Improve this script by calculating and displaying the first number raised to the power of the second number
+echo "The first number $n1 raised to the power of the second number $n2 is = " $((n1**n2))
